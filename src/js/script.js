3dd00b02f13c0d7dc05c1bf43cc9ef056e60bb1d
takuya <awa08082023@gmail.com>
2023-08-15 13:34:28 +0900

    ハンバーガーメニューjs

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,15 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+// ハンバーガーメニュー
+$('.js-hamburger').on('click', function() {
+    if($('.js-hamburger').hasClass('is-active')) {
+        $('.js-drawer').removeClass('is-active');
+        $(this).removeClass('is-active');
+    } else {
+        $('.js-drawer').addClass('is-active');
+        $(this).addClass('is-active');
+    }
+});
 
 });
