09156b293c0c6abae958d1d5f25ea4ed4c9b757c
takuya <awa08082023@gmail.com>
2023-08-25 03:23:58 +0900

    リンク先入力（スムーススクロールも記述）

--- src/js/script.js
+++ src/js/script.js
@@ -255,4 +255,17 @@ $(window).on("resize", function () {
 });
 
 
+
+  // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動)
+  $(document).on('click', 'a[href*="#"]', function () {
+    let time = 400;
+    let header = $('header').innerHeight();
+    let target = $(this.hash);
+    if (!target.length) return;
+    let targetY = target.offset().top - header;
+    $('html,body').animate({ scrollTop: targetY }, time, 'swing');
+    return false;
+  });
+
+
 });
